b11eaf9fa73833a9fce337a9407acb554d728d5c
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-08-10 13:22:55 +0200

    Add basic fragment shader

---
+++ utils/shader_compiler.cc
@@ -0,0 +1,5 @@
+//
+// Created by leo on 8/10/16.
+//
+
+#include "shader_compiler.hh"
---
+++ utils/shader_compiler.hh
@@ -0,0 +1,14 @@
+//
+// Created by leo on 8/10/16.
+//
+
+#ifndef TUTO1_SHADER_COMPILER_HH
+#define TUTO1_SHADER_COMPILER_HH
+
+
+class shader_compiler {
+
+};
+
+
+#endif //TUTO1_SHADER_COMPILER_HH
