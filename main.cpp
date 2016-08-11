b697979e2f7e33d2e07e3f992cf6115e08d4e0c1
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-08-11 17:40:12 +0200

    Draw rectangle using EBO

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 // GLEW
-#define GLEW_STATIC
 #include <GL/glew.h>
 
 #include <SFML/Window.hpp>
-#include <SFML/OpenGL.hpp>
 #include <SFML/Graphics.hpp>
 
 #include <fstream>
-#include "utils/file_reader.hh"
+#include "utils/shader_compiler.hh"
 
+void print_info();
 
 int main()
 {
@@ -20,33 +19,50 @@ int main()
     std::cout << "Glew initialized successfully" << std::endl;
   // We create a VBO
   GLfloat vertices[] = {
-      -0.5f, -0.5f, 0.0f,
+      0.5f, 0.5f, 0.0f,
       0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f,
+      -0.5f, 0.5f, 0.0f
   };
+  GLuint indices[] = {
+      0, 1, 3,
+      1, 2, 3
+  };
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  // VAO use
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  // Copy our vertices array in a buffer for OpenGL to use
   GLuint VBO; // Vertex Buffer Object
   glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT),
+                          (GLvoid*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+  glBindVertexArray(0);
   // We create a vertex shader
-  printf("OpenGL version is (%s)\n", glGetString(GL_VERSION));
-  printf("GLSL version is (%s)\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-  std::string str = file_reader::readFile("hello_triangle_vertex.glsl");
-  const GLchar *vertexShaderSource = str.c_str();
-  GLuint vertexShader;
-  vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  // Checking for compilation errors
+  GLuint vertexShader = shader_compiler::compile("hello_triangle_vertex.glsl", GL_VERTEX_SHADER);
+  GLuint fragmentShader = shader_compiler::compile("hello_triangle_fragment.glsl", GL_FRAGMENT_SHADER);
+  GLuint shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
   GLint success;
   GLchar infolog[512];
-  int bufflen = 0;
-  glCompileShader(vertexShader);
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-  glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &bufflen);
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
   if (success != GL_TRUE) {
-    glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
-    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-      << infolog << std::endl;
+    glGetShaderInfoLog(shaderProgram, 512, NULL, infolog);
+    std::cout << "ERROR::PROGRAM::LINK::LINKING_FAILED\n"
+    << infolog << std::endl;
+    return 0;
   }
   bool running = true;
   while (running) {
@@ -55,8 +71,18 @@ int main()
     while (window->pollEvent(event))
       if (event.type == sf::Event::Closed)
         running = false;
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
     window->display();
   }
   delete window;
   return 0;
+}
+
+void print_info() {
+  printf("OpenGL version is (%s)\n", glGetString(GL_VERSION));
+  printf("GLSL version is (%s)\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 }
