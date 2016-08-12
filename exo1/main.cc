2af89b98ba9414882d74ed855659a4281cffb643
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-08-12 12:47:05 +0200

    Add exo1 that displays 2 triangles

--- exo1/main.cc
+++ exo1/main.cc
@@ -16,6 +16,66 @@ void print_info();
 
 int main()
 {
+  sf::Window *window = new sf::Window(sf::VideoMode(800, 600), "Coucou",
+    sf::Style::Default, sf::ContextSettings(32));
+  window->setVerticalSyncEnabled(true);
+  if (glewInit() == GLEW_OK)
+    std::cout << "Glew initialized successfully" << std::endl;
+  GLfloat vertices[] = {
+      -0.5f, -0.5f, 0.0f,
+      0.0f, 0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      1.0f, 0.5f, 0.0f,
+      1.5f, -0.5f, 0.0f
+  };
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO); {
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+      GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT),
+                          (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+  }
+  glBindVertexArray(0);
+  GLuint vertexShader = shader_compiler::compile("hello_triangle_vertex.glsl",
+    GL_VERTEX_SHADER);
+  GLuint fragmentShader = shader_compiler::compile("hello_triangle_fragment.glsl",
+                                                 GL_FRAGMENT_SHADER);
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  GLint success;
+  GLchar infolog[512];
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (success != GL_TRUE) {
+    glGetShaderInfoLog(shaderProgram, 512, NULL, infolog);
+    std::cout << "ERROR::PROGRAM::LINK::LINKING_FAILED\n"
+    << infolog << std::endl;
+    return 0;
+  }
+  bool running = true;
+  while (running) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    sf::Event event;
+    while (window->pollEvent(event))
+      if (event.type == sf::Event::Closed)
+        running = false;
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+      glDrawArrays(GL_TRIANGLES, 0, 6);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+    window->display();
+  }
+  delete window;
   return 0;
 }
 
