b46f224f37d24e1d64910936e89e1f5587c66cc3
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-08-25 14:31:19 +0200

    [TUTO3] Fix vertex attrib pointer, finally displaying texture

--- 3_textures/tuto/main.cc
+++ 3_textures/tuto/main.cc
@@ -56,13 +56,19 @@ int main()
   GLuint VBO; // Vertex Buffer Object
   glGenBuffers(1, &VBO);
   glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof (GL_FLOAT),
-                          (GLvoid*)(6 * sizeof (GLfloat)));
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(2);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat),
+                        (GLvoid*)(0 * sizeof (GLfloat)));
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat),
+                        (GLvoid*)(3 * sizeof (GLfloat)));
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof (GLfloat),
+                        (GLvoid*)(6 * sizeof (GLfloat)));
+  glEnableVertexAttribArray(2);
   glBindVertexArray(0);
   Shader shader("tuto/shaders/vertex_shader.glsl",
                 "tuto/shaders/fragment_shader.glsl");
