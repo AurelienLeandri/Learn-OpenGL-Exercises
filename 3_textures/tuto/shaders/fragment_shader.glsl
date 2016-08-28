96821a0b5bb02563a491406ed756d059a67ec77c
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-08-29 01:26:28 +0200

    [TUTO3][EX1] Flip only the second texture

--- 3_textures/tuto/shaders/fragment_shader.glsl
+++ 3_textures/tuto/shaders/fragment_shader.glsl
@@ -6,8 +6,11 @@ uniform sampler2D uTexture2;
 in vec3 vColor;
 in vec2 vTexCoord;
 
+vec2 vTexCoordFlip;
+
 out vec4 fColor;
 
 void main() {
-    fColor = mix(texture(uTexture, vTexCoord), texture(uTexture2, vTexCoord), 0.2);
+    fColor = mix(texture(uTexture, vec2(vTexCoord.x, 1.0f - vTexCoord.y)),
+        texture(uTexture2, vTexCoord), 0.2);
 }
