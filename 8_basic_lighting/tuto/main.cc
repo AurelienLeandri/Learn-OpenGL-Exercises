befa05b100415c0ffa8f4e709ad168a8ab2f038b
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-09-11 17:12:42 +0200

    [TUTO8] Update vertex attrib pointer of lightsource to ignore its normal vectors

--- 8_basic_lighting/tuto/main.cc
+++ 8_basic_lighting/tuto/main.cc
@@ -121,9 +121,6 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
                           (GLvoid*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
-                          (GLvoid *)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
   glBindVertexArray(0);
   // We create a vertex shader
   bool running = true;
