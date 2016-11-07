18ed0ba0e2c9e5c0aa92e8ce82fa4ad45cab9e2c
Aurelien Leandri <aurelien.leandri@epita.fr>
2016-11-07 21:44:22 +0100

    [TUTO14] Add source main to load simple scene

--- 14_depth_testing/tuto/main.cc
+++ 14_depth_testing/tuto/main.cc
@@ -13,6 +13,7 @@
 #include "camera.hh"
 
 void updateCamera(Camera &camera, float deltaTime, sf::Vector2f &formerPosition);
+GLuint loadTexture(GLchar* path);
 
 int main() {
   // Creating window
@@ -23,6 +24,102 @@ int main() {
   glEnable(GL_DEPTH_TEST);
   if (glewInit() == GLEW_OK)
     std::cout << "Glew initialized successfully" << std::endl;
+  // Define the viewport dimensions
+  glViewport(0, 0, 800, 600);
+
+  // Setup some OpenGL options
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_ALWAYS); // Set to always pass the depth test (same effect as glDisable(GL_DEPTH_TEST))
+
+  // Setup and compile our shaders
+  Shader shader("../../../Path/To/Shaders/advanced.vs", "../../../Path/To/Shaders/advanced.frag");
+
+#pragma region "object_initialization"
+  // Set the object data (buffers, vertex attributes)
+  GLfloat cubeVertices[] = {
+      // Positions          // Texture Coords
+      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+      0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+      -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+      0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+  };
+  GLfloat planeVertices[] = {
+      // Positions            // Texture Coords (note we set these higher than 1 that together with GL_REPEAT as texture wrapping mode will cause the floor texture to repeat)
+      5.0f,  -0.5f,  5.0f,  2.0f, 0.0f,
+      -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+      -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+      5.0f,  -0.5f,  5.0f,  2.0f, 0.0f,
+      -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+      5.0f,  -0.5f, -5.0f,  2.0f, 2.0f
+  };
+  // Setup cube VAO
+  GLuint cubeVAO, cubeVBO;
+  glGenVertexArrays(1, &cubeVAO);
+  glGenBuffers(1, &cubeVBO);
+  glBindVertexArray(cubeVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+  glBindVertexArray(0);
+  // Setup plane VAO
+  GLuint planeVAO, planeVBO;
+  glGenVertexArrays(1, &planeVAO);
+  glGenBuffers(1, &planeVBO);
+  glBindVertexArray(planeVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+  glBindVertexArray(0);
+
+  // Load textures
+  GLuint cubeTexture = loadTexture((GLchar*)"../resources/textures/pattern4diffuseblack.jpg");
+  GLuint floorTexture = loadTexture((GLchar*)"../resources/textures/metal.png");
+#pragma endregion
+
   Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
   bool running = true;
   sf::Clock clock;
@@ -35,9 +132,55 @@ int main() {
         running = false;
     updateCamera(camera, clock.getElapsedTime().asSeconds(), mousePosition);
     clock.restart();
+    // Draw objects
+    shader.Use();
+    glm::mat4 model;
+    glm::mat4 view = camera.getViewMatrix();
+    glm::mat4 projection = glm::perspective(camera.getZoom(), (float)800/(float)600, 0.1f, 100.0f);
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+    // Cubes
+    glBindVertexArray(cubeVAO);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture);  // We omit the glActiveTexture part since TEXTURE0 is already the default active texture unit. (sampler used in fragment is set to 0 as well as default)
+    model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    model = glm::mat4();
+    model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    // Floor
+    glBindVertexArray(planeVAO);
+    glBindTexture(GL_TEXTURE_2D, floorTexture);
+    model = glm::mat4();
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
     window->display();
   }
   delete window;
+}
+
+GLuint loadTexture(GLchar* path)
+{
+  //Generate texture ID and load texture data
+  GLuint textureID;
+  glGenTextures(1, &textureID);
+  int width,height;
+  unsigned char* image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
+  // Assign texture to ID
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  // Parameters
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glBindTexture(GL_TEXTURE_2D, 0);
+  SOIL_free_image_data(image);
+  return textureID;
 }
 
 // Camera controls
